9000c3b8e6a6eea62f94739e33227089ca4aa550
Thomas Jenni <tjenni@me.com>
2022-06-20 08:12:52 +0200

    pixelbild.py eingefügt.

---
+++ pixelbild.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+width = 16
+height = 16
+
+img = Image.new( 'RGB', (width,height), "black") # Create a new black image
+pixels = img.load() # Create the pixel map
+
+pixels[5,5] = (0,255,0)
+pixels[6,5] = (255,0,0)
+pixels[5,6] = (255,0,0)
+pixels[6,6] = (255,0,0)
+
+plt.imshow(img, interpolation="none")
+plt.show()
